28ecc07457766c685ad8f84559c068fb82615856
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2020-09-23 10:11:43 -0400

    Removed wait and countown logic from game screen component

--- src/app/game-screen/game-screen.component.ts
+++ src/app/game-screen/game-screen.component.ts
@@ -10,11 +10,6 @@ import { GameSocketService } from '../services/game-socket.service';
 })
 export class GameScreenComponent implements OnInit{
   lobby_id: string;
-  timerInterval;
-  waitingInterval;
-  showCountdown: boolean = false;
-  timeUntilGameStart: number = 10;
-  waitingElipses: string = "";
   constructor(private router: Router, private route: ActivatedRoute, private gameSocket: GameSocketService) { }
 
   ngOnInit(): void {
@@ -25,34 +20,9 @@ export class GameScreenComponent implements OnInit{
       if(!inTheRightLobby) this.router.navigate(['/']);
     });
 
-    this.gameSocket.gameStart$.subscribe(() => this.startTimer());
-
-    this.startWaiting();
   }
 
   inTheRightLobby(lobbyId: string): boolean {
     return this.gameSocket.lobbyId === lobbyId;
-  }
-
-  startTimer() {
-    clearInterval(this.waitingInterval);
-    this.showCountdown = true;
-    this.timerInterval = setInterval(() => {
-      if(this.timeUntilGameStart > 0) {
-        this.timeUntilGameStart--;
-      } else {
-        clearInterval(this.timerInterval);
-      }
-    },1000)
-  }
-
-  startWaiting() {
-    this.waitingInterval = setInterval(() => {
-      if(this.waitingElipses.length === 3) {
-         this.waitingElipses = "";
-      } else {
-        this.waitingElipses += ".";
-      }
-    }, 500);
   }
 }
